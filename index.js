626205096ec9d38a03ed2b639e4cbf9ba63a53cb
abdullahahmed-abd <abdullahahmedgithub@gmail.com>
2026-02-13 12:16:57 +0530

    push

--- index.js
+++ index.js
@@ -9,42 +9,42 @@ import './global.css';
 // import { AppProvider } from './Allcomponents/ContextApi';
 import 'text-encoding-polyfill';
 import './GlobalTextOverride.jsx';
-import messaging from '@react-native-firebase/messaging'; // Import FCM
+// import messaging from '@react-native-firebase/messaging'; // Import FCM
 
-//   Add polyfill here
-if (!Array.prototype.findLastIndex) {
-  Array.prototype.findLastIndex = function (predicate, thisArg) {
-    for (let i = this.length - 1; i >= 0; i--) {
-      if (predicate.call(thisArg, this[i], i, this)) {
-        return i;
-      }
-    }
-    return -1;
-  };
-}
+// //   Add polyfill here
+// if (!Array.prototype.findLastIndex) {
+//   Array.prototype.findLastIndex = function (predicate, thisArg) {
+//     for (let i = this.length - 1; i >= 0; i--) {
+//       if (predicate.call(thisArg, this[i], i, this)) {
+//         return i;
+//       }
+//     }
+//     return -1;
+//   };
+// }
 
-//   Handle background messages
-messaging().setBackgroundMessageHandler(async remoteMessage => {
-  console.log('📨 Message handled in the background!', remoteMessage);
-});
+// //   Handle background messages
+// messaging().setBackgroundMessageHandler(async remoteMessage => {
+//   console.log('📨 Message handled in the background!', remoteMessage);
+// });
 
-//   Handle notification that opened the app (from quit)
-messaging()
-  .getInitialNotification()
-  .then(remoteMessage => {
-    if (remoteMessage) {
-      console.log('🚀 App opened from quit state via notification:', remoteMessage.notification);
-    }
-  });
+// //   Handle notification that opened the app (from quit)
+// messaging()
+//   .getInitialNotification()
+//   .then(remoteMessage => {
+//     if (remoteMessage) {
+//       console.log('🚀 App opened from quit state via notification:', remoteMessage.notification);
+//     }
+//   });
 
-//   Foreground listener
-messaging().onMessage(async remoteMessage => {
-  console.log('📩 Foreground message received:', remoteMessage);
-  Alert.alert(
-    remoteMessage.notification?.title || 'New Message',
-    remoteMessage.notification?.body || 'You have a new notification'
-  );
-});
+// //   Foreground listener
+// messaging().onMessage(async remoteMessage => {
+//   console.log('📩 Foreground message received:', remoteMessage);
+//   Alert.alert(
+//     remoteMessage.notification?.title || 'New Message',
+//     remoteMessage.notification?.body || 'You have a new notification'
+//   );
+// });
 
 //   Wrap app in context provider
 const RootApp = () => (
